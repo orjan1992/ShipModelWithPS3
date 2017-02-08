39ecf4e1175899d9deb7dc161ebc982bab6cd326
orjan1992 <orjan1992@hotmail.com>
2017-02-08 22:52:32 +0100

    Revert "Fixed noise generator added thrust allocation wth small errors"
    This reverts commit b99e2aa1478b0265c46d503f924c9a291b344d9a.

--- init.m
+++ init.m
@@ -1,12 +1,2 @@
-K_psi = 1e-20;
-Tau_in = [1 0 0]';
-
 eta_0 = [0 0 0];
 nu_0 =[0 0 0];
-
-Par.Noise.Power_pos = 0.01*(0.01);
-Par.Noise.Power_heading = 0.01*(0.01*pi/180);
-Par.Noise.Enable = 1;
-Par.Noise.Sampling_freq = 100;
-Par.Noise.Sampling_time = 1/Par.Noise.Sampling_freq;
-sim main.slx

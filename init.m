d354040886b83961887a38edc140978c54b26a50
orjan1992 <orjan1992@hotmail.com>
2017-02-11 18:49:49 +0100

    Small changes to thrust ref

--- init.m
+++ init.m
@@ -2,10 +2,11 @@ eta_0 = [0 0 0];
 nu_0 =[0 0 0];
 tau = [1.03 0 0];
 Par.SensNoiseEnabled = 1;
-ps3=0;
+Par.EnablePS3 = 0;
+
 Par.Step_size = 0.1;
 Par.Sample_time = 0.1;
 Par.Thrust_lim = [1.03 2.5 0.98]';
 
-K_psi = 0.28;
+K_psi =0.28;%0.28;
 sim main.slx
